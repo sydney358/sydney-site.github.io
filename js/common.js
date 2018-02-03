6cb641e85da416a1594f908b39be634e24cd339b
sydney <o.bova@meta.ua>
2018-02-03 20:08:37 +0200

    spirit8

---
+++ js/common.js
@@ -0,0 +1,123 @@
+/**
+ * Created by sydney on 22.01.2018.
+ */
+// -------------------menu-------------
+
+
+$(function(){
+    $('.nav').slicknav();
+});
+
+// ----------------sroll-------------------
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
+// ---------scroll_to_top----------------
+$(document).ready(function() {
+    /*
+     var defaults = {
+     containerID: 'toTop', // fading element id
+     containerHoverID: 'toTopHover', // fading element hover id
+     scrollSpeed: 500,
+     easingType: 'linear'
+     };
+     */
+
+    $().UItoTop({ easingType: 'easeOutQuart' });
+});
+// -------------------team----------------
+
+$('.team').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+// ---------------clients------------------
+$('.some_clients').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1260,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+]
+});
+// -----------------mixItUp-----------------------
+var mixer = mixitup('.portfolio_content');
+
+// -----------------testi------------
+$('.testi_content').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
